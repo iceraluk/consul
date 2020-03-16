06bd57c2731850ec1771675aeac0bab49923d2fa
Julian Herrero <microweb10@gmail.com>
2020-03-16 12:54:00 +0100

    Include duration in budgets table

--- app/components/admin/budgets/index_component.rb
+++ app/components/admin/budgets/index_component.rb
@@ -4,4 +4,24 @@ class Admin::Budgets::IndexComponent < ApplicationComponent
   def initialize(budgets)
     @budgets = budgets
   end
+
+  private
+
+    def phase_progress_text(budget)
+      t("admin.budgets.index.table_phase_progress",
+        current_phase_number: current_enabled_phase_number(budget),
+        total_phases: budget.phases.enabled.count)
+    end
+
+    def current_enabled_phase_number(budget)
+      budget.phases.enabled.order(:id).pluck(:kind).index(budget.phase) + 1
+    end
+
+    def start_date(budget)
+      l(budget.phases.first.starts_at, format: :long) if budget.phases.first.starts_at.present?
+    end
+
+    def end_date(budget)
+      l(budget.phases.last.ends_at, format: :long) if budget.phases.last.ends_at.present?
+    end
 end

caf5b702c8d7c12049d1932fb37d8a85ab8392ba
decabeza <alberto@decabeza.es>
2020-03-16 12:54:00 +0100

    Change budget phases design

--- app/components/budgets/phases_component.rb
+++ app/components/budgets/phases_component.rb
@@ -5,4 +5,22 @@ class Budgets::PhasesComponent < ApplicationComponent
   def initialize(budget)
     @budget = budget
   end
+
+  private
+
+    def phases
+      budget.published_phases
+    end
+
+    def phase_name(phase)
+      t("budgets.phase.#{phase.kind}")
+    end
+
+    def start_date(phase)
+      l(phase.starts_at.to_date, format: :long) if phase.starts_at.present?
+    end
+
+    def end_date(phase)
+      l(phase.ends_at.to_date - 1.day, format: :long) if phase.ends_at.present?
+    end
 end
